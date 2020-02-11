ed947a39f42b0f40f2f5d057e765f5b52b5c169e
JaredDyreson <jared.dyreson@gmail.com>
2020-02-10 23:25:24 -0800

    wrote a simpler hex parser

---
+++ hex_parser/hex_to_ascii.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#define IN true
+#define OUT false
+
+void parse(int value, FILE* out){
+  const char table[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+  int base = 10;
+
+  if(value >= 10 && value <= 16){
+    int index = (value - base)-1;
+    if(index >= 0){  
+      fprintf(out, "%c\n", table[index]);
+    }
+  }
+  else{
+    fprintf(out, "%d\n", value);
+  }
+}
+
+int main(int argc, const char* argv[]){
+  FILE* output = fopen("hex_output", "w");
+  for(int i = 0; i < 17; ++i){
+    parse(i, output);
+  }
+  fclose(output);
+  return 0;
+}
