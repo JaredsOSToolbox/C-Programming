710243cd642ac5fd5385c06cc698f24f8761da19
JaredDyreson <jared.dyreson@gmail.com>
2020-03-18 15:45:27 -0700

    added the exam code

--- exam_one/code_for_exam.c
+++ exam_one/code_for_exam.c
@@ -1,8 +1,154 @@
 #include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Written by Jared Dyreson
 // All rights reserved
 
+void count(){
+  printf("Enter lines of text here: ....\n");
+  int c = 0, nl = 0, nc = 0, nt = 0, nbackslash = 0;
+  while((c = fgetc(stdin)) != EOF){
+    if(c == '\n'){ ++nl; }
+    else if(c == '\t'){ ++nt; }
+    else if(c == '\\'){ ++nbackslash; }
+    else if(isalpha(c)){ ++nc; }
+  }
+  printf("lines: %d\n", nl);
+  printf("tabs: %d\n", nt);
+  printf("backslashes: %d\n", nbackslash);
+  printf("alphanumeric: %d\n", nc);
+}
+
+// Identify the SL functions that do the following
+// (2) Find the first character c in a const char* s ------> strchr (<string.h>)
+// (3) Find the length of the prefix of const char* s that has characters in const char* t --------> 
+// (4) Terminates a program immediately ------> exit (<stdlib.h>)
+// (5) Open the file "midterm.txt" in read mode ---> fopen
+
+
+
+// BEGIN STRING.H IMPLEMENTATION
+char* strcpy_(char* s, const char* t){
+  char* p = s;
+  while((*t != '\0') && (*s++ = *t++)){}
+  return p;
+}
+
+void test_strcpy_(){
+  char hello[strlen("Hello")];
+  memset(hello, 0, sizeof(char));
+  printf("%s\n", strcpy_(hello, "Hello"));
+}
+
+char* strncat_(char* s, const char* t, size_t n){
+  char* ptr = s + strlen(s);
+  while((*ptr++ = *t++) && (*s != '\0') && (--n > 0)){}
+  *ptr = '\0';
+  return s;
+}
+
+void test_strncat_(){
+  char destination[30];
+  strcpy(destination, "Hello");
+  char source[] = "World";
+  printf("contents of t: %s\n", strncat_(destination, source, 5));
+}
+
+int strcmp_(const char* s, const char* t){
+  while(*s++ == *t++ && (*s != '\0')){}
+  return *s - *t;
+}
+
+void test_strcmp(){
+  const char a[] = "Hello";
+  const char b[] = "Hello";
+  int value = strcmp_(a, b);
+  printf("value of value: %d\n", value);
+  if(value == 0){
+    printf("they are the same!\n");
+  }
+  else if(value > 1){
+    printf("a is bigger than b\n");
+  }
+  else{
+    printf("b is bigger than a\n");
+  }
+}
+
+// END STRING.H IMPLEMENTATION
+
+
+
+
+// BEGIN SQUARE ADT IMPLEMENTATION
+
+typedef struct point point;
+struct point{ double x, y; };
+
+typedef struct square square;
+struct square{ point ul; size_t side; };
+
+square* square_constructor(double ulx, double uly, double side){
+  square* s = (square *)malloc(sizeof(square));
+  s->ul.x = ulx;
+  s->ul.y = uly;
+  s->side = side;
+  return s;
+}
+
+void square_destructor(square* s){ free(s); }
+
+void square_move(square* sq, double x, double y){
+  // updates position
+  sq->ul.x = x;
+  sq->ul.y = y;
+}
+
+void square_expandby(square* sq, double lambda){
+  // scale the square by a constant lambda
+  sq->ul.x*=lambda;
+  sq->ul.y*=lambda;
+  sq->side*=lambda;
+}
+
+double square_area(square* sq){ return sq->side * sq->side; }
+double square_perimeter(square* sq){ return 4 * sq->side; }
+
+void square_repr(square* sq){
+  printf("Coordinate: (%g, %g)\nSide length: %zu\nArea: %g\nPerimeter: %g\n",
+            sq->ul.x, sq->ul.y, sq->side, square_area(sq), square_perimeter(sq));
+}
+
+void test_square(double ulx, double uly, double side){
+  printf("Constructing sqaure...\n");
+  printf("==========================\n");
+  square* s = square_constructor(ulx, uly, side);
+  square_repr(s);
+  printf("==========================\n");
+  printf("Updating coordinate to (2, 2)\n");
+  printf("==========================\n");
+  square_move(s, 2, 2);
+  square_repr(s);
+  printf("===============================\n");
+  printf("Expanding squre by lambda = 10\n");
+  printf("===============================\n");
+  square_expandby(s, 10);
+  square_repr(s);
+  printf("==========================\n");
+  square_destructor(s);
+  printf("\n\n");
+}
+
+void square_tests(){
+  test_square(0, 0, 10);
+  test_square(1, 1, 5);
+}
+
+// END SQUARE ADT IMPLEMENTATION
+
 int main(){
+  square_tests();
   return 0;
 }
--- rectangle/rectangle.c
+++ rectangle/rectangle.c
@@ -6,46 +6,75 @@ typedef struct rectangle rectangle;
 typedef struct point point;
 
 struct point { double x, y; };
+point* point_constructor(double x, double y){
+  point* p = (point *)malloc(sizeof(point));
+  p->x =  x;
+  p->y =  y;
+  return p;
+}
+
+void point_destructor(point* p){ free(p); }
+
+
 struct rectangle {  point ul;  point br;  };
 
-rectangle* rectangle_init(double ulx, double uly, double brx, double bry){
+void rectangle_update_cooridnates(rectangle* r, point* ul_r, point* br_r){
+  // give the rectangle new coordinates
+  r->ul.x = ul_r->x;
+  r->ul.y = ul_r->y;
+  r->br.x = br_r->x;
+  r->br.y = br_r->y;
+}
+
+rectangle* rectangle_constructor(point* ul, point* br){
   rectangle* rect = (rectangle *)malloc(sizeof(rectangle));
-  rect->ul.x = ulx;
-  rect->ul.y = uly;
-  rect->br.x = brx;
-  rect->br.y = bry;
+  rectangle_update_cooridnates(rect, ul, br);
   return rect;
 }
+
+double length_(rectangle* r){ return fabs(r->ul.x - r->br.x); }
+double width_(rectangle* r){ return fabs(r->ul.y - r->br.y); }
+
 void rectangle_destructor(rectangle* r){ free(r); }
 
-void rectangle_move(rectangle* r, double x, double y){
-  r->ul.x =x;
-  r->ul.y =y;
-  r->br.x =x;
-  r->br.y =y;
+void rectangle_move(rectangle* r, double alpha, double beta){
+  // move the rectangle horizontally by a constant alpha
+  // move the rectangl vertically by a constant beta
+  r->ul.x+=alpha;
+  r->ul.y+=beta;
+  r->br.x+=alpha;
+  r->br.y+=beta;
 }
-void rectangle_expandby(rectangle* r, double expandby){
-  r->ul.x*=expandby;
-  r->ul.y*=expandby;
-  r->br.x*=expandby;
-  r->br.y*=expandby;
-
+void rectangle_expandby(rectangle* r, double lambda){
+  // dialate the rectangle by a constant lambda
+  r->ul.x*=lambda;
+  r->ul.y*=lambda;
+  r->br.x*=lambda;
+  r->br.y*=lambda;
 }
 
 double rectangle_area(rectangle* r){
-  double l = fabs(r->ul.x - r->br.x);
-  double w = fabs(r->ul.y - r->br.y);
-  return l * w;
+  // compute the area of the rectangle in R^2
+  return (length_(r) * width_(r));
 }
 
 double rectangle_perimeter(rectangle* r){
-  double l = fabs(r->ul.x - r->br.x);
-  double w = fabs(r->ul.y - r->br.y);
-  return 2 * (l+w);
+  // compute the perimeter of the rectangle in R^2
+  return 2 * (length_(r)+width_(r));
+}
+
+void rectangle_repr(rectangle* r){
+  printf("Upper left: (%g, %g)\nBottom right: (%g, %g)", 
+          r->ul.x, r->ul.y, r->br.x, r->br.y);
 }
 
 int main(){
-  rectangle* r = rectangle_init(1, 1, 2, 2);
+  point* upper = point_constructor(0, 1);
+  point* bottom = point_constructor(2, 0);
+  rectangle* r = rectangle_constructor(upper, bottom);
+  rectangle_repr(r);
   rectangle_destructor(r);
+  point_destructor(upper);
+  point_destructor(bottom);
   return 0;
 }
